b8f025cf870e19fc845a47018f1ed9c53622a6f6
221B0825 <eunseoyu0825@gmail.com>
2024-06-17 16:12:39 +0900

    feat: add sub menu structure

--- AutomotiveComplaintSystem/src/controller/MainComplaintController.java
+++ AutomotiveComplaintSystem/src/controller/MainComplaintController.java
@@ -19,7 +19,7 @@ public class MainComplaintController {
 	public String login() {
 		LoginResult result = this.loginController.login();
 		if (result.getMessage().equals(BasicMessage.LoginSuccess.getMessage())) {
-			loginUser = result.getLoginUser();
+			this.loginUser = result.getLoginUser();
 			return result.getMessage();
 		} else {
 			return result.getMessage();
@@ -42,30 +42,111 @@ public class MainComplaintController {
 
 		while (true) {
 			// show MainMenu
-			this.TUI.showMainMenu();
+			this.TUI.printMainMenu();
 			String selected = view.DataInput.sc.nextLine();
 			switch (selected) {
+			// 등록민원 신청
 			case "1":
-
+				registerationComplain();
 				break;
+			// 발급/열람 민원 신청
 			case "2":
-
+				issuanceComplain();
 				break;
+			// 사용자 신청 승인
 			case "3":
 
 				break;
+			// 납부
 			case "4":
 
 				break;
+			// 로그아웃
 			case "5":
 				TUI.printLogoutMessage(loginUser.getName());
 				return;
 
 			default:
+				TUI.printWrongInputMessage();
 				break;
 			}
 
 		}
+	}
+
+	// 등록 민원 신청 처리
+	private void registerationComplain() {
+		this.TUI.printRegistrationComplaintMenu();
+
+		String selected = view.DataInput.sc.nextLine();
+		switch (selected) {
+		// 자동차 신규등록 신청
+		case "1":
+
+			break;
+		// 자동차 변경등록 신청
+		case "2":
+
+			break;
+		// 자동차 양도증명
+		case "3":
+
+			break;
+		// 자동차 이전등록신청
+		case "4":
+
+			break;
+		// 저당권설정등록신청
+		case "5":
+
+			break;
+		// 자동차 말소등록신청
+		case "6":
+
+			break;
+		// 정기/종합검사 유효기간 연장(유예)신청
+		case "7":
+
+			break;
+
+		default:
+			TUI.printWrongInputMessage();
+			break;
+		}
+	}
+
+	// 발급 민원 신청 처리
+	private void issuanceComplain() {
+		this.TUI.printInissuanceComplainMenu();
+		String selected = view.DataInput.sc.nextLine();
+
+		switch (selected) {
+		// 자동차 등록증 재발급신청
+		case "1":
+
+			break;
+		// 자동차 등록원부 등본(초본) 발급·열람신청	
+		case "2":
+
+			break;
+		// 자동차 말소사실증명서 발급신청
+		case "3":
+
+			break;
+		// 이륜차 사용폐지증명서 발급신청	
+		case "4":
+
+			break;
+		// 이륜차 사용신고필증 재발급신청	
+		case "5":
+
+			break;
+
+		default:
+			TUI.printWrongInputMessage();
+			break;
+		}
+
 	}
 
 }
--- AutomotiveComplaintSystem/src/view/TUI.java
+++ AutomotiveComplaintSystem/src/view/TUI.java
@@ -29,13 +29,25 @@ public class TUI {
 		System.out.println("잘못된 입력입니다.");
 	}
 
-	public void showMainMenu() {
-		System.out.println("1.등록 민원 신청 2.발급 민원 신청 3.신청 승인 4.납부 5.로그아웃");
+	public void printMainMenu() {
+		System.out.println("1.등록 민원 신청 2.발급/열람 민원 신청 3.신청 승인 4.납부 5.로그아웃");
 		System.out.print("입력: ");
 	}
-	
+
+	public void printRegistrationComplaintMenu() {
+		System.out.println(
+				"1.자동차 신규등록신청 2.자동차 변경등록신청 3.자동차 양도증명 4.자동차 이전등록신청 5.저당권설정등록신청 6.자동차 말소등록신청 7.정기/종합검사 유효기간 연장(유예)신청");
+		System.out.print("입력: ");
+	}
+
+	public void printInissuanceComplainMenu() {
+		System.out.println(
+				"1.자동차 등록증 재발급신청 2.자동차 등록원부 등본(초본) 발급·열람신청 3.자동차 말소사실증명서 발급신청 4.이륜차 사용폐지증명서 발급신청 5.이륜차 사용신고필증 재발급신청");
+		System.out.print("입력: ");
+	}
+
 	public void printLogoutMessage(String userName) {
-		System.out.println(userName+" :: "+BasicMessage.Logout.getMessage());
+		System.out.println(userName + " :: " + BasicMessage.Logout.getMessage());
 	}
 
 	public void printExit() {
